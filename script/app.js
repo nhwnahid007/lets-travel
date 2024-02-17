cfacf56a6e6a5da97958945e501326b6c5ad0fee
nhwnahid007 <nhwnahid@gmail.com>
2024-02-17 18:06:13 +0600

    Completed grand total section with bus train flight calculation

--- script/app.js
+++ script/app.js
@@ -11,20 +11,20 @@ let count=0;
 const allBtn= document.getElementsByClassName('add-btn'); //button clik korle select korbe 
 // console.log(allBtn);
 
-//>->all btn er sokol class k loop korbo jeno click korle sob pawa jai
+//>>->all btn er sokol class k loop korbo jeno click korle sob pawa jai
 
 
 
 for (const btn of allBtn){
     // console.log(btn);
     btn.addEventListener('click', function(event){
-      //>->event click korle 1 kore count barbe
+      //>>->event click korle 1 kore count barbe
       // console.log('boss select korsi');
       count = count + 1;
-      // console.log(count); //>->jotobar click korbo barbe
+      // console.log(count); //>>->jotobar click korbo barbe
 
-      //>->cart er 0 k add korbe tai innertext a count set korbo
-      setInnerText("cart-count", count); //>->setInnerText function k call kore id and value pass kore dilam
+      //>>->cart er 0 k add korbe tai innertext a count set korbo
+    //   setInnerText("cart-count", count); //>>->setInnerText function k call kore id and value pass kore dilam
 
       //#jetake ami click kortesi seta holo target means ami jodi event.target likhi jeita click korchi oita
 
@@ -36,12 +36,12 @@ for (const btn of allBtn){
 
       const price = event.target.parentNode.childNodes[3].childNodes[1].innerText;
       //   console.log(price);
-      //! >-> ekhane shudhu 300 k change korbo $ k korbo na ejnno h4 er chilnode 300 k select kora hoise
+      //! >>-> ekhane shudhu 300 k change korbo $ k korbo na ejnno h4 er chilnode 300 k select kora hoise
+ 
 
+      //>>->event.target.parentNode.childNodes
 
-      //>->event.target.parentNode.childNodes
-
-      //>->parentNode div theke childnodes console.log korle child nodes gulo dekhay ..segulo expand korle nodelist dekhay and index dekkhay ..ami name change korbo tai Shajek lekha h2 er index bosiye inner text korlam tai ota show korlo.. eta passi tai etake ekta variable a rakhbo
+      //>>->parentNode div theke childnodes console.log korle child nodes gulo dekhay ..segulo expand korle nodelist dekhay and index dekkhay ..ami name change korbo tai Shajek lekha h2 er index bosiye inner text korlam tai ota show korlo.. eta passi tai etake ekta variable a rakhbo
 
 
       const selectedContainer=document.getElementById('selected-place-container');
@@ -57,28 +57,52 @@ for (const btn of allBtn){
 
       selectedContainer.appendChild(li)
 
-      //>-> prothome jeta main cotainer jekhane price and place show korbo setar id dhoresi then li create kore li er vetore 2 ta p tag create kore p tag k li er moddhe appendchild kore rekhe diyesi ..then li k main container a appendChild kore dilam
+      //>>-> prothome jeta main cotainer jekhane price and place show korbo setar id dhoresi then li create kore li er vetore 2 ta p tag create kore p tag k li er moddhe appendchild kore rekhe diyesi ..then li k main container a appendChild kore dilam
 
-        //>-> simply total cost ber korbo
-        const totalCost = document.getElementById('total-cost').innerText;
-        console.log(typeof totalCost); //string thakbe number a convert korte hbe
-        const convertedTotalCost=parseInt(totalCost);
-
-
-        document.getElementById("total-cost").innerText=convertedTotalCost+ parseInt(price);
-
-        //>-> total cost er jaigai price set korrte hobe ..ejnno price k parseInt kora hoyese karon price age string silo 
-        //>-> total price ber korte total cost er inner text k convertedtotatlcost+price kore jog kora hoise
+        //>>-> simply total cost ber korbo
+    //     const totalCost = document.getElementById('total-cost').innerText;
+    //     // console.log(typeof totalCost);
+    //    //string thakbe number a convert korte hbe
+    //     const convertedTotalCost=parseInt(totalCost);
 
 
+    //     const sum = convertedTotalCost+ parseInt(price);
 
+    //#alter
+
+    totalCost('total-cost', parseInt(price))
+
+
+        // document.getElementById("total-cost").innerText=sum; 
+
+        //otar ^ poriborte nice function call kore o kora jabe <total-cost holo id || sum holo value>>-
+
+       
+
+        //>>-> total cost er jaigai price set korrte hobe ..ejnno price k parseInt kora hoyese karon price age string silo 
+        //>>-> total price ber korte total cost er inner text k convertedtotatlcost+price kore jog kora hoise
+
+        // const grandTotal = document.getElementById("grand-total").innerText;
+        // const convertedGrandTotal=parseInt(grandTotal);
+
+        // const sum2=convertedGrandTotal+parseInt(price);
+
+        //ekahne sum + korle total sum soho add hoto 
+
+
+        grandTotalCost("grand-total", parseInt(price))
+
+        // console.log( convertedGrandTotal);
+
+        //  setInnerText('grand-total',sum2)
+         setInnerText("cart-count", count);
     })
 }
 
-//>->amra eitar moddhe onek innertext add korbo tai set innertext er arekta function kore nite pari
+//>>->amra eitar moddhe onek innertext add korbo tai set innertext er arekta function kore nite pari
 
 
-//>->jar inner text change korbo tar id nibo and new value er jnno value parameter nilam
+//>>->jar inner text change korbo tar id nibo and new value er jnno value parameter nilam
 
 //!id jehutu string call korar somoy string id diye call korte hbe
 
@@ -88,3 +112,49 @@ function setInnerText(id,value) {
 
 //totalcost er function
 
+function totalCost(id,value) {
+    const totalCost = document.getElementById(id).innerText;
+    const convertedTotalCost = parseInt(totalCost);
+    const sum = convertedTotalCost + parseInt(value);
+    // setInnerText(id, sum);
+    setInnerText('total-cost', sum);
+
+}
+
+// function grandTotalCost() {
+//   const totalCost = document.getElementById("grand-total").innerText;
+//   const convertedTotalCost = parseInt(totalCost);
+//   const sum = convertedTotalCost + parseInt(value);
+//   setInnerText(id, sum);
+//   setInnerText('grand-total', sum);
+
+// }
+
+function grandTotalCost(category){
+
+  const totalCost = document.getElementById("total-cost").innerText;
+  const convertedTotalCost = parseInt(totalCost);
+
+  // console.log(category);
+  if (category === "bus") {
+    setInnerText("grand-total", convertedTotalCost+100);
+  }
+  else if (category === "train") {
+    setInnerText("grand-total", convertedTotalCost-200);
+  }
+  
+  else if (category === "flight") {
+    setInnerText("grand-total", convertedTotalCost+500);
+  }
+  else{
+    setInnerText("grand-total", convertedTotalCost);
+  }
+  
+  //grand total finction call hole se total cost er 0 k dhorbe
+  //    const totalCost = document.getElementById("total-cost").innerText;
+  //     //totalCost dorlam karon oitai add korbo then grand total korbo
+  //     const convertedTotalCost=parseInt(totalCost);
+
+  //     setInnerText('grand-total', convertedTotalCost);
+  //function call na korle print hobe na
+}
